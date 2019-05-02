cf80a6532ec6145dd5d190001e3d4b8f3ab40d26
lidongkui <908564167@qq.com>
2019-05-02 19:16:06 +0800

    每日一道

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,18 +1,31 @@
 package test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        map.put(1,1);
-        map.put(5,5);
-        map.put(4,4);
-        map.put(3,3);
-        map.put(2,2);
-        for(Integer key : map.keySet()){
-            System.out.println(key);
+        Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<2;i++){
+            list.add(scanner.nextLine());
+
+        }
+        char []c = {'E','A','S','Y'};
+        for (String s : list) {
+            int flag = 0;
+            for(int i=0;i<s.length();i++){
+                if(c[flag] == s.charAt(i)){
+                    flag++;
+                }
+
+                if(flag == 4){
+                    System.out.println("easy");
+                    break;
+                }
+            }
+            if(flag != 4){
+                System.out.println("difficult");
+            }
         }
 
     }
